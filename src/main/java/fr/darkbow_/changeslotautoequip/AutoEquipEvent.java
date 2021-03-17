b1afcb65afe76ab1e0d62e830044a6f166bd730a
BuildTools <unconfigured@null.spigotmc.org>
2021-03-17 17:05:13 +0100

    Slot

--- src/main/java/fr/darkbow_/changeslotautoequip/AutoEquipEvent.java
+++ src/main/java/fr/darkbow_/changeslotautoequip/AutoEquipEvent.java
@@ -15,10 +15,12 @@ public class AutoEquipEvent implements Listener {
         Player player = event.getPlayer();
         if(player.getScoreboardTags().contains("ChangingSlotAutoEquip")){
             if(player.getInventory().getItem(event.getNewSlot()) == null || Objects.requireNonNull(player.getInventory().getItem(event.getNewSlot())).getType() == Material.AIR){
-                player.getInventory().setHelmet(null);
-                player.getInventory().setChestplate(null);
-                player.getInventory().setLeggings(null);
-                player.getInventory().setBoots(null);
+                if(player.getInventory().getItem(event.getPreviousSlot()) != null && Objects.requireNonNull(player.getInventory().getItem(event.getPreviousSlot())).getType() != Material.AIR){
+                    player.getInventory().setHelmet(null);
+                    player.getInventory().setChestplate(null);
+                    player.getInventory().setLeggings(null);
+                    player.getInventory().setBoots(null);
+                }
             } else if(Objects.requireNonNull(player.getInventory().getItem(event.getNewSlot())).getType().name().contains("SWORD") || Objects.requireNonNull(player.getInventory().getItem(event.getNewSlot())).getType().name().contains("AXE")){
                 player.getInventory().setHelmet(player.getInventory().getItem(9));
                 player.getInventory().setChestplate(player.getInventory().getItem(10));
