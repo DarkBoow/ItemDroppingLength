db796e09692bc2901fd818663553eed6c89299c7
BuildTools <unconfigured@null.spigotmc.org>
2021-03-17 17:34:30 +0100

    Slot

--- src/main/java/fr/darkbow_/changeslotautoequip/AutoEquipEvent.java
+++ src/main/java/fr/darkbow_/changeslotautoequip/AutoEquipEvent.java
@@ -16,16 +16,68 @@ public class AutoEquipEvent implements Listener {
         if(player.getScoreboardTags().contains("ChangingSlotAutoEquip")){
             if(player.getInventory().getItem(event.getNewSlot()) == null || Objects.requireNonNull(player.getInventory().getItem(event.getNewSlot())).getType() == Material.AIR){
                 if(player.getInventory().getItem(event.getPreviousSlot()) != null && Objects.requireNonNull(player.getInventory().getItem(event.getPreviousSlot())).getType() != Material.AIR){
-                    player.getInventory().setHelmet(null);
-                    player.getInventory().setChestplate(null);
-                    player.getInventory().setLeggings(null);
-                    player.getInventory().setBoots(null);
+                    boolean helmet = false;
+                    boolean chestplate = false;
+                    boolean leggings = false;
+                    boolean boots = false;
+
+                    if(player.getInventory().getHelmet() != null && player.getInventory().getHelmet().getType() != Material.AIR){
+                        helmet = true;
+                        if(player.getInventory().getItem(9) == null || player.getInventory().getItem(9).getType() == Material.AIR){
+                            player.getInventory().setItem(9, player.getInventory().getHelmet());
+                        } else {
+                            player.getInventory().addItem(player.getInventory().getHelmet());
+                        }
+                    }
+
+                    if(player.getInventory().getChestplate() != null && player.getInventory().getChestplate().getType() != Material.AIR){
+                        chestplate = true;
+                        if(player.getInventory().getItem(10) == null || player.getInventory().getItem(10).getType() == Material.AIR){
+                            player.getInventory().setItem(10, player.getInventory().getChestplate());
+                        } else {
+                            player.getInventory().addItem(player.getInventory().getChestplate());
+                        }
+                    }
+
+                    if(player.getInventory().getLeggings() != null && player.getInventory().getLeggings().getType() != Material.AIR){
+                        leggings = true;
+                        if(player.getInventory().getItem(11) == null || player.getInventory().getItem(11).getType() == Material.AIR){
+                            player.getInventory().setItem(11, player.getInventory().getLeggings());
+                        } else {
+                            player.getInventory().addItem(player.getInventory().getLeggings());
+                        }
+                    }
+
+                    if(player.getInventory().getBoots() != null && player.getInventory().getBoots().getType() != Material.AIR){
+                        boots = true;
+                        if(player.getInventory().getItem(12) == null || player.getInventory().getItem(12).getType() == Material.AIR){
+                            player.getInventory().setItem(12, player.getInventory().getBoots());
+                        } else {
+                            player.getInventory().addItem(player.getInventory().getBoots());
+                        }
+                    }
+
+                    if(helmet){player.getInventory().setHelmet(null);}
+                    if(chestplate){player.getInventory().setChestplate(null);}
+                    if(leggings){player.getInventory().setLeggings(null);}
+                    if(boots){player.getInventory().setBoots(null);}
+
+                    if(helmet || chestplate || leggings || boots){player.updateInventory();}
                 }
             } else if(Objects.requireNonNull(player.getInventory().getItem(event.getNewSlot())).getType().name().contains("SWORD") || Objects.requireNonNull(player.getInventory().getItem(event.getNewSlot())).getType().name().contains("AXE")){
                 player.getInventory().setHelmet(player.getInventory().getItem(9));
+                player.getInventory().remove(Objects.requireNonNull(player.getInventory().getItem(9)));
+
                 player.getInventory().setChestplate(player.getInventory().getItem(10));
+                player.getInventory().remove(Objects.requireNonNull(player.getInventory().getItem(10)));
+
                 player.getInventory().setLeggings(player.getInventory().getItem(11));
+                player.getInventory().remove(Objects.requireNonNull(player.getInventory().getItem(11)));
+
                 player.getInventory().setBoots(player.getInventory().getItem(12));
+                player.getInventory().remove(Objects.requireNonNull(player.getInventory().getItem(12)));
+
+                player.updateInventory();
             }
         }
     }
